56bea1f0d462323267dadde45dcae6789bf4ed7a
iguxa <tsybykov@bk.ru>
2018-09-24 00:17:06 +0300

    'test'

---
+++ 1.1.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: it-iguxa
+ * Date: 2018-08-28
+ * Time: 14:15
+ */
+function distributeSmoothies(int $m ,int $n):int {
+    $result = $m/$n;
+    return $result;
+}
+echo distributeSmoothies(7,3);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<p>new change</p>
+
+</body>
+</html>
